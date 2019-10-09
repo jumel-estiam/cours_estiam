7515af4400a17441e39657c6ba7a9a7a5a97ad22
Vincent-Xavier JUMEL <endymion@thetys-retz.net>
2019-10-09 15:13:47 +0200

    Passage à une fonction récursive
    Signed-off-by: Vincent-Xavier JUMEL <endymion@thetys-retz.net>

--- flocon.py
+++ flocon.py
@@ -1,22 +1,25 @@
 from turtle import *
 
-def flocon(l):
-    motif(l)
+def flocon(n,l):
+    motif(n,l)
     left(120)
-    motif(l)
+    motif(n,l)
     left(120)
-    motif(l)
+    motif(n,l)
     left(120)
 
 
-def motif(l):
-    forward(l/3)
-    right(60)
-    forward(l/3)
-    left(120)
-    forward(l/3)
-    right(60)
-    forward(l/3)
+def motif(n, l):
+    if n == 0:
+        forward(l)
+    else:
+        motif(n-1,l/3)
+        right(60)
+        motif(n-1,l/3)
+        left(120)
+        motif(n-1,l/3)
+        right(60)
+        motif(n-1,l/3)
 
 
-flocon(120)
+flocon(2,120)
