910f173ead12b18c33a75491e179b90db4c9c88a
jumel-estiam <56260714+jumel-estiam@users.noreply.github.com>
2019-10-09 14:55:53 +0200

    Import initial
    Première itération du flocon

---
+++ flocon.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+def flocon(l):
+    motif(l)
+    left(120)
+    motif(l)
+    left(120)
+    motif(l)
+    left(120)
+    
+    
+def motif(l)
+    forward(l/3)
+    right(60)
+    forward(l/3)
+    left(120)
+    forward(l/3)
+    right(60)
+    forward(l/3)
+    
+flocon(120)
