1ec63509b1d0ed55c5a8a7aeb8b478bb826d3ac1
Vincent-Xavier JUMEL <endymion@thetys-retz.net>
2019-10-09 16:18:19 +0200

    La version antiflocon
    Signed-off-by: Vincent-Xavier JUMEL <endymion@thetys-retz.net>

---
+++ antiflocon.py
@@ -0,0 +1,28 @@
+from turtle import *
+
+def flocon(n,l):
+    motif(n,l)
+    left(120)
+    motif(n,l)
+    left(120)
+    motif(n,l)
+    left(120)
+
+
+def motif(n, l):
+    if n == 0:
+        forward(l)
+    else:
+        motif(n-1,l/3)
+        left(60)
+        motif(n-1,l/3)
+        right(120)
+        motif(n-1,l/3)
+        left(60)
+        motif(n-1,l/3)
+
+
+ht()
+tracer(8,25)
+speed(0)
+flocon(7,729/3)
